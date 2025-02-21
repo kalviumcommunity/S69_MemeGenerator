07d39f4eeb414a409894751b0afccc34ab652284
DIYA <diya94816@gmail.com>
2025-02-21 21:26:16 +0530

    add files

---
+++ client/src/components/MemeCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MemeCard = () => {
+   
+    
+
+
+  return (
+    <div>
+      <h2>MEME OF THE DAY: WHEN YOU FINALLY UNDERSTAND GITHUB</h2>
+      <img src="https://i.imgflip.com/30b1gx.jpg" height="300px"  alt="Meme" />
+    </div>
+  );
+};
+
+export default MemeCard;
